ec5248c5f115c5af469072e6dc046235deb81d88
omidrahmati2000 <omidrahmati2000@gmail.com>
2019-11-07 02:56:51 -0800

    done faze 1

--- edu/views.py
+++ edu/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'edu/index.html')
