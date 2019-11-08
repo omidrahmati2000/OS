7c0a42b89c235d86503738cfdb24126035474bde
omidrahmati2000 <omidrahmati2000@gmail.com>
2019-11-08 00:13:10 -0800

    some change

--- edu/admin.py
+++ edu/admin.py
@@ -1,1 +1,8 @@
 from django.contrib import admin
+
+from edu import models
+
+
+@admin.register(models.Course)
+class CourseAdmin(admin.ModelAdmin):
+    pass
--- edu/models.py
+++ edu/models.py
@@ -1,3 +1,16 @@
+from django.db import models
 from django.contrib.auth.models import User
 
+
+class Course(models.Model):
+    department = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    course_number = models.CharField(max_length=200)
+    group_number = models.CharField(max_length=200)
+    teacher = models.CharField(max_length=200)
+    start_time = models.CharField(max_length=200)
+    end_time = models.CharField(max_length=200)
+    day= (('1','1'), ('2','2'), ('3','3'), ('4','4'))
+    first_day = models.CharField(max_length=200,choices=day)
+    second_day = models.CharField(max_length=200, choices=day)
 
